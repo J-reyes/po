bc3bf133126125ddb04de8452fad856348d76851
Jose Reyes <vazquez.jr09@gmail.com>
2018-11-08 18:43:24 -0800

    ignore file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Login from './components/login'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Login />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;

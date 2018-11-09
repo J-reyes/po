52976e8ed33966f8b09b41bceb527dfa6e928654
Jose Reyes <vazquez.jr09@gmail.com>
2018-11-08 19:08:07 -0800

    pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import Login from './components/login'
-import logo from './logo.svg';
+import Login from './components/login';
+import Register from './components/register';
+import Home from './components/home';
+
+import {Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Login />
-        </header>
+        <Switch>
+         <Route exact path="/" component={Login}/>
+         <Route path="/register" component={Register} />
+         <Route path="/home" component={Home}/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.setState = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Register</h1>
+            </div>
+        )
+    }
+}

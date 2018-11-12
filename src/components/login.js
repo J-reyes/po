4965f7d8c61d23f3e521c043ab7de7b71908e864
Jose Reyes <vazquez.jr09@gmail.com>
2018-11-12 13:45:48 -0800

    styling

--- src/components/login.js
+++ src/components/login.js
@@ -9,11 +9,25 @@ class Login extends React.Component {
     }
     render() {
         return (
-             <div className='container'>
-                <h1>Login page</h1>
-             </div>
+            <div className="container" id="login-container">
+                <div className="App" id="login">
+                    <div className="card">
+                     <div className="card-body">
+                        <h2 className="card-title">Login</h2>
+                        <div>
+                            <label htmlFor="" id="user-label">User Name</label>
+                            <input type="text" />
+                            <label htmlFor="" id="password-label">Password</label>
+                            <input type="text"/>
+                        </div>
+                        <hr/>
+                        <button href="#" className="card-link" id="sign-in-text">Sign In</button>
+                        <button href="#" className="card-link" id="register-text">Register</button>
+                     </div>
+                    </div>
+                </div>
+            </div>
         );
     }
-
 }
 export default Login;

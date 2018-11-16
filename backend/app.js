96a467b435c0c234d19839805c7ff3d4d1170315
Jose Reyes <vazquez.jr09@gmail.com>
2018-11-15 20:28:40 -0800

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ var userRouter = require("./routes/users");
 
 
 var app = express();
+
 app.use(logger("short"));
 app.use(mongomorgan("mongodb://localhost:27017/po", "dev"));
 
@@ -29,8 +30,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.user(cookieParser());
-app.user(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 // API Routes
 app.use("/users", userRouter);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,11 @@ const userController = require('../controllers/user-controller');
 
 // get all users
 router.get('/', userController.index);
+router.get('/userid/:id', userController.getById)
+
+router.delete('/:id', userController.destroy);
 
 // post new user
 router.post('/new', userController.createUser);
+
+module.exports = router;

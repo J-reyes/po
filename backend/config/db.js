388744cf0f43d5a5c2cc653cc0fae50920a6aa50
Reuben Bagtas <reubenbagtas.com>
2018-11-22 08:59:29 -0800

    slight changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var path = require('path'); //added
-var fs = require('fs'); //added
 
 mongoose.Promise = global.Promise;
 
@@ -10,14 +9,4 @@ mongoose.connection.once('open', function () {
     console.log('Mongoose connected');
 })
 
-//added=================================
-//create a variable that points to the path where all the models live
-var models_path = path.join(__dirname, './../models'); 
-fs.readdirSync(models_path).forEach(function(file) {
-    if(file.indexOf('.js') >= 0) {
-      // require the file (this runs the model file which registers the schema)
-      require(models_path + '/' + file);
-    }
-  });
-//======================================
 module.exports = mongoose;
